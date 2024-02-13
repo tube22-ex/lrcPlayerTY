9f5cfd765785e52195112fa40c7833bebafb3959
tube22-ex <fodata@outlook.jp>
2024-02-13 22:05:13 +0900

    no message

--- web/js/typing.js
+++ web/js/typing.js
@@ -12,6 +12,17 @@ const disp = ()=>{
     yomiA.innerText = keygraph.seq_done();
 }
 
+const dataDisp = ()=>{
+    status_miss.textContent = typesDataObj["Miss"];
+    status_types.textContent = typesDataObj["Types"];
+    status_clearLines.textContent = typesDataObj["lineClertCnt"];
+    status_score.textContent = (Math.round((typesDataObj["Score"] / 2000)*100)/100).toFixed(2); 
+    status_lost.textContent = typesDataObj["Lost"];
+    status_kps.textContent = typesDataObj["AllSpeed"].toFixed(2);
+    status_acc.textContent = typesDataObj["Acc"].toFixed(2);
+    status_lines.textContent = typesDataObj["LineRemaining"]
+}
+
 let DataAry = [];
 let lineDataObj = {
     Types:0,
